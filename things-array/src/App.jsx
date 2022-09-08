170e884edaef03edb7b4522b6a6fd4c33e261f8f
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-09-08 22:50:37 +0100

    update

--- things-array/src/App.jsx
+++ things-array/src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 
 function App() {
-  const thingsArray = ["Thing 1", "Thing 2"]
+  const [thingsArray, setThingsArray] = React.useState(["Thing 1", "Thing 2"])
+  
   const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
   
 function addItem() {
-
+  setThingsArray(prevThingsArray => [...prevThingsArray, `Things ${thingsArray.length + 1}`])
 }
 
   
