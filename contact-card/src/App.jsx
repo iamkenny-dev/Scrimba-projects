320cff9f3b03f2da533141f7894e00b6176aa74e
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-09-08 13:18:08 +0100

    contact-card

---
+++ contact-card/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Card from './Card'
+
+export default function App() {
+  return (
+    <div>
+      <Card 
+                img="./images/mr-whiskerson.png" 
+                name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+      />
+      <Card 
+                img="./images/fluffykins.png"
+                name="Fluffykins"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+      />
+      <Card 
+                img="./images/felix.png"
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+      />
+      <Card 
+                img="./images/pumpkin.png"
+                name="Pumpkin"
+                phone="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
+      />
+    </div>
+  )
+}
